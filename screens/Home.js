d6f247c6e8e4135c71048a6f57afbfaeb475458d
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2022-07-19 12:21:00 +0700

    color

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, SafeAreaView} from 'react-native';
 import React from 'react';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView></ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ themes/color.js
@@ -0,0 +1,6 @@
+export default {
+  white: '#ffff',
+  black: '#000',
+  blueAqua: '#3D90B6',
+  redMaroon: '#D52028',
+};
